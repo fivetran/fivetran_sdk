bcf4f4f6683bad0d3897759f62a34b6089bc09a3
emrah.diril <emrah.diril@fivetran.com>
2023-11-11 18:44:34 -0500

    No need to save docker image as tar file

--- tools/scripts/build_sdk_connector_tester_image.sh
+++ tools/scripts/build_sdk_connector_tester_image.sh
@@ -10,7 +10,5 @@ cp -f "$(git rev-parse --show-toplevel)/tools/bazel-bin/testers/run_sdk_connecto
 
 docker build -t sdk-connector-tester -f Dockerfile.connector_tester --platform=linux/amd64 .
 
-docker save --output sdk-connector-tester.tar sdk-connector-tester
-
 # clean up
 rm run_sdk_connector_tester_deploy.jar
--- tools/scripts/build_sdk_destination_tester_image.sh
+++ tools/scripts/build_sdk_destination_tester_image.sh
@@ -10,7 +10,5 @@ cp -f "$(git rev-parse --show-toplevel)/tools/bazel-bin/testers/run_sdk_destinat
 
 docker build -t sdk-destination-tester -f Dockerfile.destination_tester --platform=linux/amd64 .
 
-docker save --output sdk-destination-tester.tar sdk-destination-tester
-
 # clean up
 rm run_sdk_destination_tester_deploy.jar
