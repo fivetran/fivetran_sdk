42583b8310c78e2e2661c4acd99d901904504f2c
emrah.diril <emrah.diril@fivetran.com>
2023-11-17 17:20:10 -0500

    Fix alter_table

--- tools/testers/src/testers/SdkDestinationTester.java
+++ tools/testers/src/testers/SdkDestinationTester.java
@@ -59,7 +59,7 @@ import picocli.CommandLine;
 public final class SdkDestinationTester {
     private static final Logger LOG = Logger.getLogger(SdkDestinationTester.class.getName());
 
-    private static final String VERSION = "2023.1103.1752";
+    private static final String VERSION = "2023.1116.1120";
 
     private static final CsvMapper CSV = createCsvMapper();
     private static final String DEFAULT_SCHEMA = "tester";
@@ -220,7 +220,7 @@ public final class SdkDestinationTester {
 
         // alterTable (needs to be fully defined)
         if (batch.containsKey("alter_table")) {
-            for (var tableEntry : ((Map<String, Object>) batch.get("create_table")).entrySet()) {
+            for (var tableEntry : ((Map<String, Object>) batch.get("alter_table")).entrySet()) {
                 String tableName = tableEntry.getKey();
 
                 if (!tableDMLs.containsKey(tableName)) {
