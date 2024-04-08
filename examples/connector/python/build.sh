3a5d24cc4bb2be1d6b5175ff9c18cd74946905ec
Manjunath Tapali <manjunath@fivetran.com>
2024-04-08 11:43:08 +0530

    fix(python-example): refomatted folder structure

--- examples/connector/python/build.sh
+++ examples/connector/python/build.sh
@@ -2,7 +2,7 @@
 python3 -m venv connector_run
 source connector_run/bin/activate
 mkdir -p protos
-cp ../../../../*.proto protos/
+cp ../../../*.proto protos/
 pip install -r requirements.txt
 mkdir -p sdk_pb2
 python -m grpc_tools.protoc \
