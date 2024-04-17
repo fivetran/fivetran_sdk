0ae8d038500d9648ba2d996f585b261ffe9bd2f0
fivetran-abdulsalam <abdul.salam@fivetran.com>
2024-04-17 13:06:38 +0530

    fix python destination example

--- examples/destination/python/main.py
+++ examples/destination/python/main.py
@@ -2,6 +2,7 @@ from concurrent import futures
 import grpc
 import read_csv
 import sys
+
 sys.path.append('sdk_pb2')
 
 from sdk_pb2 import destination_sdk_pb2
@@ -9,18 +10,27 @@ from sdk_pb2 import common_pb2
 from sdk_pb2 import destination_sdk_pb2_grpc
 
 
-class DestinationImpl(destination_sdk_pb2_grpc.DestinationServicer):
+class DestinationImpl(destination_sdk_pb2_grpc.DestinationConnectorServicer):
     def ConfigurationForm(self, request, context):
 
-        host = common_pb2.FormField(name="host", label="Host", required=True,
-                                     text_field=common_pb2.TextField.PlainText)
-        password = common_pb2.FormField(name="password", label="Password", required=True,
-                                         text_field=common_pb2.TextField.Password)
-        region = common_pb2.FormField(name="region", label="AWS Region", required=False,
-                                       dropdown_field=common_pb2.DropdownField(dropdown_field=["US-EAST", "US-WEST"]))
-        hidden = common_pb2.FormField(name="hidden", label="my-hidden-value", text_field=common_pb2.TextField.Hidden)
-        is_public = common_pb2.FormField(name="isPublic", label="Public?", description="Is this public?",
-                                          toggle_field=common_pb2.ToggleField())
+        host = common_pb2.FormField(
+            single=common_pb2.Field(name="host", label="Host", required=True, placeholder="my.example.host",
+                                    text_field=common_pb2.TextField.PlainText))
+
+        password = common_pb2.FormField(
+            single=common_pb2.Field(name="password", label="Password", required=True, placeholder="my_password",
+                                    text_field=common_pb2.TextField.Password))
+
+        region = common_pb2.FormField(
+            single=common_pb2.Field(name="region", label="AWS Region", required=False, default_value="US-EAST",
+                                    dropdown_field=common_pb2.DropdownField(dropdown_field=["US-EAST", "US-WEST"])))
+
+        hidden = common_pb2.FormField(
+            common_pb2.Field(name="hidden", label="my-hidden-value", text_field=common_pb2.TextField.Hidden))
+
+        is_public = common_pb2.FormField(
+            single=common_pb2.Field(name="isPublic", label="Public?", description="Is this public?",
+                                    toggle_field=common_pb2.ToggleField()))
 
         connect_test = common_pb2.ConfigurationTest(name="connect", label="Tests connection")
         select_test = common_pb2.ConfigurationTest(name="select", label="Tests selection")
@@ -30,8 +40,7 @@ class DestinationImpl(destination_sdk_pb2_grpc.DestinationServicer):
             fields=[host, password, region, hidden,
                     is_public],
             tests=[connect_test, select_test]
-
-            )
+        )
 
     def Test(self, request, context):
         test_name = request.name
@@ -39,17 +48,50 @@ class DestinationImpl(destination_sdk_pb2_grpc.DestinationServicer):
         return common_pb2.TestResponse(success=True)
 
     def CreateTable(self, request, context):
-        print("[CreateTable] :" + str(request.schema_name) + " | " + str(request.table.name) + " | " + str(request.table.columns))
+        print("[CreateTable] :" + str(request.schema_name) + " | " + str(request.table.name) + " | " + str(
+            request.table.columns))
         return destination_sdk_pb2.CreateTableResponse(success=True)
 
     def AlterTable(self, request, context):
-        res: destination_sdk_pb2.AlterTableResponse
-
-        print("[AlterTable]: " + str(request.schema_name) + " | " + str(request.table.name) + " | " + str(request.table.columns))
+        changes_list = request.changes
+        change_strings = [str(change) for change in changes_list]
+        result = ", ".join(change_strings)
+        print("[AlterTable]: " + str(request.schema_name) + " | " + str(request.table_name) + " | " + str(
+            result))
         return destination_sdk_pb2.AlterTableResponse(success=True)
 
+    def Capabilities(self, request, context):
+        destination_map_to_1 = destination_sdk_pb2.DestinationType(name="VARCHAR",
+                                                                   map_to=destination_sdk_pb2.DestinationType.map_to.STRING)
+        data_type_mapping_1 = destination_sdk_pb2.DataTypeMappingEntry(
+            fivetran_type=common_pb2.DataType.STRING,
+            map_to=destination_map_to_1)
+
+        destination_map_to_2 = destination_sdk_pb2.DestinationType(name="NUMBER",
+                                                                   map_to=destination_sdk_pb2.DestinationType.map_to.INT)
+        data_type_mapping_2 = destination_sdk_pb2.DataTypeMappingEntry(
+            fivetran_type=common_pb2.DataType.FLOAT,
+            map_to=destination_map_to_2)
+
+        destination_map_to_3 = destination_sdk_pb2.DestinationType(name="DATE",
+                                                                   map_to=destination_sdk_pb2.DestinationType.map_to.UTC_DATETIME)
+        data_type_mapping_3 = destination_sdk_pb2.DataTypeMappingEntry(
+            fivetran_type=common_pb2.DataType.UTC_DATETIME,
+            map_to=destination_map_to_3)
+
+        destination_map_to_4 = destination_sdk_pb2.DestinationType(name="BLOB",
+                                                                   map_to=destination_sdk_pb2.DestinationType.map_to.BINARY)
+        data_type_mapping_4 = destination_sdk_pb2.DataTypeMappingEntry(
+            fivetran_type=common_pb2.DataType.BINARY,
+            map_to=destination_map_to_4)
+
+        return destination_sdk_pb2.CapabilitiesResponse(
+            data_type_mappings=[data_type_mapping_1, data_type_mapping_2, data_type_mapping_3, data_type_mapping_4],
+            supports_history_mode=True)
+
     def Truncate(self, request, context):
-        print("[TruncateTable]: " + str(request.schema_name) + " | " + str(request.schema_name) + " | soft" + str(request.soft))
+        print("[TruncateTable]: " + str(request.schema_name) + " | " + str(request.schema_name) + " | soft" + str(
+            request.soft))
         return destination_sdk_pb2.TruncateResponse(success=True)
 
     def WriteBatch(self, request, context):
@@ -81,7 +123,7 @@ class DestinationImpl(destination_sdk_pb2_grpc.DestinationServicer):
 if __name__ == '__main__':
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     server.add_insecure_port('[::]:50052')
-    destination_sdk_pb2_grpc.add_DestinationServicer_to_server(DestinationImpl(), server)
+    destination_sdk_pb2_grpc.add_DestinationConnectorServicer_to_server(DestinationImpl(), server)
     server.start()
     print("Destination gRPC server started...")
     server.wait_for_termination()
