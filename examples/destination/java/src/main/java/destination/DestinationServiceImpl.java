cfe413478f5d36a74ac8e713288fac3b6615b5fd
Niket Khandelwal <niket.khandelwal@fivetran.com>
2024-05-17 12:29:35 +0530

    feature(partner_sdk): fix java examples as per updated proto files (#33)

--- examples/destination/java/src/main/java/destination/DestinationServiceImpl.java
+++ examples/destination/java/src/main/java/destination/DestinationServiceImpl.java
@@ -1,12 +1,15 @@
 package destination;
 
+import com.google.common.collect.Lists;
+import com.google.protobuf.AbstractMessage;
 import fivetran_sdk.*;
 import io.grpc.stub.StreamObserver;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class DestinationServiceImpl extends DestinationGrpc.DestinationImplBase {
+public class DestinationServiceImpl extends DestinationConnectorGrpc.DestinationConnectorImplBase {
     @Override
     public void configurationForm(ConfigurationFormRequest request, StreamObserver<ConfigurationFormResponse> responseObserver) {
         responseObserver.onNext(
@@ -15,27 +18,56 @@ public class DestinationServiceImpl extends DestinationGrpc.DestinationImplBase 
                         .setTableSelectionSupported(true)
                         .addAllFields(Arrays.asList(
                                 FormField.newBuilder()
-                                        .setName("host").setLabel("Host").setRequired(true).setTextField(TextField.PlainText).build(),
-                                FormField.newBuilder()
-                                        .setName("password").setLabel("Password").setRequired(true).setTextField(TextField.Password).build(),
-                                FormField.newBuilder()
-                                        .setName("region").setLabel("AWS Region").setRequired(false).setDropdownField(
-                                                DropdownField.newBuilder().addAllDropdownField(
-                                                        Arrays.asList("US-EAST", "US-WEST")).build()
-                                        ).build(),
-                                FormField.newBuilder()
-                                        .setName("hidden").setLabel("my-hidden-value").setTextField(TextField.Hidden)
+                                        .setSingle(Field.newBuilder().setName("host").setLabel("Host").setPlaceholder("my.example.host")
+                                                .setRequired(true).setTextField(TextField.PlainText).build())
                                         .build(),
                                 FormField.newBuilder()
-                                        .setName("isPublic")
-                                        .setLabel("Public?")
-                                        .setDescription("Is this public?")
-                                        .setToggleField(ToggleField.newBuilder().build())
+                                        .setSingle(Field.newBuilder().setName("password").setLabel("Password").setPlaceholder("p4ssw0rd")
+                                                .setRequired(true).setTextField(TextField.Password).build())
+                                        .build(),
+                                FormField.newBuilder()
+                                        .setSingle(Field.newBuilder().setName("region").setLabel("AWS Region").setDefaultValue("US-EAST").setRequired(false)
+                                                .setDropdownField(DropdownField.newBuilder().addAllDropdownField(Arrays.asList("US-EAST", "US-WEST")).build())
+                                                .build())
+                                        .build(),
+                                FormField.newBuilder()
+                                        .setSingle(Field.newBuilder().setName("hidden").setLabel("my-hidden-value").setTextField(TextField.Hidden).build())
+                                        .build(),
+                                FormField.newBuilder()
+                                        .setSingle(Field.newBuilder().setName("isPublic").setLabel("Public?").setDescription("Is this public?")
+                                                .setToggleField(ToggleField.newBuilder().build())
+                                                .build())
                                         .build()
-                        ))
-                        .addAllTests(Arrays.asList(
+                        )).addAllTests(Arrays.asList(
                                 ConfigurationTest.newBuilder().setName("connect").setLabel("Tests connection").build(),
                                 ConfigurationTest.newBuilder().setName("select").setLabel("Tests selection").build()))
+                        .build());
+
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void capabilities(CapabilitiesRequest request, StreamObserver<CapabilitiesResponse> responseObserver) {
+        responseObserver.onNext(
+                CapabilitiesResponse.newBuilder()
+                        .setSupportsHistoryMode(false)
+                        .addAllDataTypeMappings(Lists.newArrayList(
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.UNSPECIFIED).setMapTo(DestinationType.newBuilder().setName("UNKNOWN").setMapTo(DataType.UNSPECIFIED).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.BOOLEAN).setMapTo(DestinationType.newBuilder().setName("BOOL").setMapTo(DataType.BOOLEAN).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.SHORT).setMapTo(DestinationType.newBuilder().setName("SHORT_INTEGER").setMapTo(DataType.SHORT).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.INT).setMapTo(DestinationType.newBuilder().setName("INTEGER").setMapTo(DataType.INT).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.LONG).setMapTo(DestinationType.newBuilder().setName("LONG_INTEGER").setMapTo(DataType.LONG).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.DECIMAL).setMapTo(DestinationType.newBuilder().setName("DECIMAL").setMapTo(DataType.DECIMAL).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.FLOAT).setMapTo(DestinationType.newBuilder().setName("FLOAT").setMapTo(DataType.FLOAT).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.DOUBLE).setMapTo(DestinationType.newBuilder().setName("DOUBLE").setMapTo(DataType.DOUBLE).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.NAIVE_TIME).setMapTo(DestinationType.newBuilder().setName("TIME").setMapTo(DataType.NAIVE_TIME).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.NAIVE_DATE).setMapTo(DestinationType.newBuilder().setName("DATE").setMapTo(DataType.NAIVE_DATE).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.NAIVE_DATETIME).setMapTo(DestinationType.newBuilder().setName("DATETIME").setMapTo(DataType.NAIVE_DATETIME).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.UTC_DATETIME).setMapTo(DestinationType.newBuilder().setName("UTC_DATETIME").setMapTo(DataType.UTC_DATETIME).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.BINARY).setMapTo(DestinationType.newBuilder().setName("BLOB").setMapTo(DataType.BINARY).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.XML).setMapTo(DestinationType.newBuilder().setName("XML").setMapTo(DataType.XML).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.STRING).setMapTo(DestinationType.newBuilder().setName("VARCHAR").setMapTo(DataType.STRING).build()).build(),
+                                DataTypeMappingEntry.newBuilder().setFivetranType(DataType.JSON).setMapTo(DestinationType.newBuilder().setName("OBJECT").setMapTo(DataType.JSON).build()).build()))
                         .build());
 
         responseObserver.onCompleted();
@@ -84,7 +116,8 @@ public class DestinationServiceImpl extends DestinationGrpc.DestinationImplBase 
         Map<String, String> configuration = request.getConfigurationMap();
 
         System.out.println("[AlterTable]: " +
-                request.getSchemaName() + " | " + request.getTable().getName() + " | " + request.getTable().getColumnsList());
+                request.getSchemaName() + " | " + request.getTableName() + " | " +
+                request.getChangesList().stream().map(AbstractMessage::toString).collect(Collectors.joining(", ")));
         responseObserver.onNext(AlterTableResponse.newBuilder().setSuccess(true).build());
         responseObserver.onCompleted();
     }
