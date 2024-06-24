e30d7eb2ce669e2b2dad624ff6d29dbbac1738af
Manjunath Tapali <manjunath@fivetran.com>
2024-06-25 02:04:04 +0530

    Revert "fix(Destination): Merge file params into one (#54)"
    This reverts commit ad9b9d2032a0a7a4b4b89941688f8c767f86b131.

--- examples/destination/python/main.py
+++ examples/destination/python/main.py
@@ -150,13 +150,6 @@ class DestinationImpl(destination_sdk_pb2_grpc.DestinationConnectorServicer):
             read_csv.decrypt_file(key, value)
         print("\nData loading completed for table " + request.table.name + "\n")
 
-        print("----------\nPrinting file Params for batch files:\n")
-        print("\nCompression: " + str(request.file_params.compression))
-        print("\nEncryption: " + str(request.file_params.encryption))
-        print("\nValue for null_string: " + str(request.file_params.null_string))
-        print("\nValue for unmodified_string: " + str(request.file_params.unmodified_string))
-        print("\n----------\n")
-
         res: destination_sdk_pb2.WriteBatchResponse = destination_sdk_pb2.WriteBatchResponse(success=True)
         return res
 
