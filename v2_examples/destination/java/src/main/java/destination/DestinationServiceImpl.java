82f7b7b7831eee6491e34d76108323a0e4b5a08e
SatvikPatil <54502802+fivetran-satvikpatil@users.noreply.github.com>
2025-01-15 12:02:04 +0530

    Partner SDK: Destination examples for V2 protos (#81)
    * v2_examples folder
    
    * java example update
    
    * change the setup form
    
    * python example changes
    
    * remove v1 proto from v2_examples
    
    * remove unnecessary file
    
    * change port used
    
    * name and label changes
    
    * added comment
    
    ---------
    
    Co-authored-by: Satvik Patil <satvik.patil@satvik.patil-W4LTPGX1Q0>

---
+++ v2_examples/destination/java/src/main/java/destination/DestinationServiceImpl.java
@@ -0,0 +1,248 @@
+package destination;
+
+import fivetran_sdk.v2.*;
+import io.grpc.stub.StreamObserver;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class DestinationServiceImpl extends DestinationConnectorGrpc.DestinationConnectorImplBase {
+
+    private final String INFO = "INFO";
+    private final String WARNING = "WARNING";
+    private final String SEVERE = "SEVERE";
+
+    @Override
+    public void configurationForm(ConfigurationFormRequest request, StreamObserver<ConfigurationFormResponse> responseObserver) {
+        logMessage(INFO, "Fetching configuration form");
+        responseObserver.onNext(getConfigurationForm());
+
+        responseObserver.onCompleted();
+    }
+
+    private ConfigurationFormResponse getConfigurationForm() {
+
+        FormField writerType = FormField.newBuilder()
+                .setName("writerType")
+                .setLabel("Writer Type")
+                .setDescription("Choose the destination type")
+                .setDropdownField(
+                        DropdownField.newBuilder()
+                                .addAllDropdownField(Arrays.asList("Database", "File", "Cloud"))
+                                .build())
+                .setDefaultValue("Database")
+                .build();
+
+        FormField host = FormField.newBuilder()
+                .setName("host")
+                .setLabel("Host")
+                .setTextField(TextField.PlainText)
+                .setPlaceholder("your_host_details")
+                .build();
+
+        FormField port = FormField.newBuilder()
+                .setName("port")
+                .setLabel("Port")
+                .setTextField(TextField.PlainText)
+                .setPlaceholder("your_port_details")
+                .build();
+
+        FormField user = FormField.newBuilder()
+                .setName("user")
+                .setLabel("User")
+                .setTextField(TextField.PlainText)
+                .setPlaceholder("user_name")
+                .build();
+
+        FormField password = FormField.newBuilder()
+                .setName("password")
+                .setLabel("password")
+                .setTextField(TextField.Password)
+                .setPlaceholder("your_password")
+                .build();
+
+        FormField database = FormField.newBuilder()
+                .setName("database")
+                .setLabel("Database")
+                .setTextField(TextField.PlainText)
+                .setPlaceholder("your_database_name")
+                .build();
+
+        FormField table = FormField.newBuilder()
+                .setName("table")
+                .setLabel("Table")
+                .setTextField(TextField.PlainText)
+                .setPlaceholder("your_table_name")
+                .build();
+
+        FormField filePath = FormField.newBuilder()
+                .setName("filePath")
+                .setLabel("File Path")
+                .setTextField(TextField.PlainText)
+                .setPlaceholder("your_file_path")
+                .build();
+
+        FormField region = FormField.newBuilder()
+                .setName("region")
+                .setLabel("Cloud Region")
+                .setDescription("Choose the cloud region")
+                .setDropdownField(
+                        DropdownField.newBuilder()
+                                .addAllDropdownField(Arrays.asList("Azure", "AWS", "Google Cloud"))
+                                .build())
+                .setDefaultValue("Azure")
+                .build();
+
+        FormField enableEncryption = FormField.newBuilder()
+                .setName("enableEncryption")
+                .setDescription("To enable/disable encryption for data transfer")
+                .setLabel("Enable Encryption?")
+                .setToggleField(ToggleField.newBuilder().build())
+                .build();
+
+        // List of Visibility Conditions
+        VisibilityCondition visibilityConditionForCloud = VisibilityCondition.newBuilder()
+                .setConditionField("writerType")
+                .setStringValue("Cloud")
+                .build();
+
+        VisibilityCondition visibilityConditionForDatabase = VisibilityCondition.newBuilder()
+                .setConditionField("writerType")
+                .setStringValue("Database")
+                .build();
+
+        VisibilityCondition visibilityConditionForFile = VisibilityCondition.newBuilder()
+                .setConditionField("writerType")
+                .setStringValue("File")
+                .build();
+
+        // List of conditional fields
+        // Note: The 'name' and 'label' parameters in the FormField for conditional fields are not used.
+        FormField conditionalFieldForCloud = FormField.newBuilder()
+                .setName("conditionalFieldForCloud")
+                .setLabel("Conditional Field for Cloud")
+                .setConditionalFields(
+                        ConditionalFields.newBuilder()
+                                .setCondition(visibilityConditionForCloud)
+                                .addAllFields(Arrays.asList(host, port, user, password, region))
+                                .build())
+                .build();
+
+        FormField conditionalFieldForFile = FormField.newBuilder()
+                .setName("conditionalFieldForFile")
+                .setLabel("Conditional Field for File")
+                .setConditionalFields(
+                        ConditionalFields.newBuilder()
+                                .setCondition(visibilityConditionForFile)
+                                .addAllFields(Arrays.asList(host, port, user, password, table, filePath))
+                                .build())
+                .build();
+
+        FormField conditionalFieldForDatabase = FormField.newBuilder()
+                .setName("conditionalFieldForDatabase")
+                .setLabel("Conditional Field for Database")
+                .setConditionalFields(
+                        ConditionalFields.newBuilder()
+                                .setCondition(visibilityConditionForDatabase)
+                                .addAllFields(Arrays.asList(host, port, user, password, database,  table))
+                                .build())
+                .build();
+
+        return ConfigurationFormResponse.newBuilder()
+                .setSchemaSelectionSupported(true)
+                .setTableSelectionSupported(true)
+                .addAllFields(
+                        Arrays.asList(
+                                writerType,
+                                conditionalFieldForFile,
+                                conditionalFieldForCloud,
+                                conditionalFieldForDatabase,
+                                enableEncryption))
+                .addAllTests(
+                        Arrays.asList(
+                                ConfigurationTest.newBuilder().setName("connect").setLabel("Tests connection").build(),
+                                ConfigurationTest.newBuilder().setName("select").setLabel("Tests selection").build()))
+                .build();
+    }
+
+    @Override
+    public void test(TestRequest request, StreamObserver<TestResponse> responseObserver) {
+        Map<String, String> configuration = request.getConfigurationMap();
+        String testName = request.getName();
+        String message = String.format("Test Name: %s", testName);
+        logMessage(INFO, message);
+
+        responseObserver.onNext(TestResponse.newBuilder().setSuccess(true).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void describeTable(DescribeTableRequest request, StreamObserver<DescribeTableResponse> responseObserver) {
+        Map<String, String> configuration = request.getConfigurationMap();
+
+        DescribeTableResponse response = DescribeTableResponse.newBuilder()
+                .setTable(
+                        Table.newBuilder()
+                                .setName(request.getTableName())
+                                .addAllColumns(
+                                Arrays.asList(
+                                        Column.newBuilder().setName("a1").setType(DataType.UNSPECIFIED).setPrimaryKey(true).build(),
+                                        Column.newBuilder().setName("a2").setType(DataType.DOUBLE).build())
+                        ).build()).build();
+
+        responseObserver.onNext(response);
+        logMessage(SEVERE, "Sample Severe log: Completed describe Table method");
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void createTable(CreateTableRequest request, StreamObserver<CreateTableResponse> responseObserver) {
+        Map<String, String> configuration = request.getConfigurationMap();
+
+        String message = "[CreateTable]: "
+                + request.getSchemaName() + " | " + request.getTable().getName() + " | " + request.getTable().getColumnsList();
+        logMessage(INFO, message);
+        responseObserver.onNext(CreateTableResponse.newBuilder().setSuccess(true).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void alterTable(AlterTableRequest request, StreamObserver<AlterTableResponse> responseObserver) {
+        Map<String, String> configuration = request.getConfigurationMap();
+
+        String message = "[AlterTable]: " +
+                request.getSchemaName() + " | " + request.getTable().getName() + " | " + request.getTable().getColumnsList();
+        logMessage(INFO, message);
+        responseObserver.onNext(AlterTableResponse.newBuilder().setSuccess(true).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void truncate(TruncateRequest request, StreamObserver<TruncateResponse> responseObserver) {
+        System.out.printf("[TruncateTable]: %s | %s | soft=%s%n",
+                request.getSchemaName(), request.getTableName(), request.hasSoft());
+        responseObserver.onNext(TruncateResponse.newBuilder().setSuccess(true).build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void writeBatch(WriteBatchRequest request, StreamObserver<WriteBatchResponse> responseObserver) {
+        String message = "[WriteBatch]: " + request.getSchemaName() + " | " + request.getTable().getName();
+        logMessage(WARNING, String.format("Sample severe message: %s", message));
+        for (String file : request.getReplaceFilesList()) {
+            System.out.println("Replace files: " + file);
+        }
+        for (String file : request.getUpdateFilesList()) {
+            System.out.println("Update files: " + file);
+        }
+        for (String file : request.getDeleteFilesList()) {
+            System.out.println("Delete files: " + file);
+        }
+        responseObserver.onNext(WriteBatchResponse.newBuilder().setSuccess(true).build());
+        responseObserver.onCompleted();
+    }
+
+    private void logMessage(String level, String message){
+        System.out.println(String.format("{\"level\":\"%s\", \"message\": \"%s\", \"message-origin\": \"sdk_destination\"}", level, message));
+    }
+}
---
+++ v2_examples/destination/python/README.md
@@ -0,0 +1,15 @@
+# Python Destination Example
+
+## Pre-requisites
+- Python 3.9 or later
+
+## Steps
+- Run the build.sh file to copy protos, install python dependencies in virtual environment
+```commandline
+sh build.sh
+```
+
+- Execute `run.sh` to run the connector
+```commandline
+sh run.sh
+```
