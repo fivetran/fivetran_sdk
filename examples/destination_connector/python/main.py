125e32ecf43d3f97be04346b37385247ebefaf9f
SatvikPatil <54502802+fivetran-satvikpatil@users.noreply.github.com>
2025-03-01 23:43:14 +0530

    Fix import in python example (#100)
    * fix import
    
    * source connector examples
    
    ---------
    
    Co-authored-by: Satvik Patil <satvik.patil@satvik.patil-W4LTPGX1Q0>

--- examples/destination_connector/python/main.py
+++ examples/destination_connector/python/main.py
@@ -4,119 +4,119 @@ import read_csv
 import sys
 sys.path.append('sdk_pb2')
 
-from sdk_pb2 import destination_sdk_v2_pb2
-from sdk_pb2 import common_v2_pb2
-from sdk_pb2 import destination_sdk_v2_pb2_grpc
+from sdk_pb2 import destination_sdk_pb2
+from sdk_pb2 import common_pb2
+from sdk_pb2 import destination_sdk_pb2_grpc
 
 
 INFO = "INFO"
 WARNING = "WARNING"
 SEVERE = "SEVERE"
 
-class DestinationImpl(destination_sdk_v2_pb2_grpc.DestinationConnectorServicer):
+class DestinationImpl(destination_sdk_pb2_grpc.DestinationConnectorServicer):
     def ConfigurationForm(self, request, context):
         log_message(INFO, "Fetching Configuration form")
 
         # Create the form fields
-        form_fields = common_v2_pb2.ConfigurationFormResponse(
+        form_fields = common_pb2.ConfigurationFormResponse(
             schema_selection_supported=True,
             table_selection_supported=True
         )
 
         # writerType field with dropdown
-        writer_type = common_v2_pb2.FormField(
+        writer_type = common_pb2.FormField(
             name="writerType",
             label="Writer Type",
             description="Choose the destination type",
-            dropdown_field=common_v2_pb2.DropdownField(dropdown_field=["Database", "File", "Cloud"]),
+            dropdown_field=common_pb2.DropdownField(dropdown_field=["Database", "File", "Cloud"]),
             default_value="Database"
         )
 
         # host field
-        host = common_v2_pb2.FormField(
+        host = common_pb2.FormField(
             name="host",
             label="Host",
-            text_field=common_v2_pb2.TextField.PlainText,
+            text_field=common_pb2.TextField.PlainText,
             placeholder="your_host_details"
         )
 
         # port field
-        port = common_v2_pb2.FormField(
+        port = common_pb2.FormField(
             name="port",
             label="Port",
-            text_field=common_v2_pb2.TextField.PlainText,
+            text_field=common_pb2.TextField.PlainText,
             placeholder="your_port_details"
         )
 
         # user field
-        user = common_v2_pb2.FormField(
+        user = common_pb2.FormField(
             name="user",
             label="User",
-            text_field=common_v2_pb2.TextField.PlainText,
+            text_field=common_pb2.TextField.PlainText,
             placeholder="user_name"
         )
 
         # password field
-        password = common_v2_pb2.FormField(
+        password = common_pb2.FormField(
             name="password",
             label="Password",
-            text_field=common_v2_pb2.TextField.Password,
+            text_field=common_pb2.TextField.Password,
             placeholder="your_password"
         )
 
         # database field
-        database = common_v2_pb2.FormField(
+        database = common_pb2.FormField(
             name="database",
             label="Database",
-            text_field=common_v2_pb2.TextField.PlainText,
+            text_field=common_pb2.TextField.PlainText,
             placeholder="your_database_name"
         )
 
         # table field
-        table = common_v2_pb2.FormField(
+        table = common_pb2.FormField(
             name="table",
             label="Table",
-            text_field=common_v2_pb2.TextField.PlainText,
+            text_field=common_pb2.TextField.PlainText,
             placeholder="your_table_name"
         )
 
         # filePath field
-        file_path = common_v2_pb2.FormField(
+        file_path = common_pb2.FormField(
             name="filePath",
             label="File Path",
-            text_field=common_v2_pb2.TextField.PlainText,
+            text_field=common_pb2.TextField.PlainText,
             placeholder="your_file_path"
         )
 
         # region field with dropdown
-        region = common_v2_pb2.FormField(
+        region = common_pb2.FormField(
             name="region",
             label="Cloud Region",
             description="Choose the cloud region",
-            dropdown_field=common_v2_pb2.DropdownField(dropdown_field=["Azure", "AWS", "Google Cloud"]),
+            dropdown_field=common_pb2.DropdownField(dropdown_field=["Azure", "AWS", "Google Cloud"]),
             default_value="Azure"
         )
 
         # enableEncryption toggle field
-        enable_encryption = common_v2_pb2.FormField(
+        enable_encryption = common_pb2.FormField(
             name="enableEncryption",
             label="Enable Encryption?",
             description="To enable/disable encryption for data transfer",
-            toggle_field=common_v2_pb2.ToggleField()
+            toggle_field=common_pb2.ToggleField()
         )
 
         # Define Visibility Conditions for Conditional Fields
-        visibility_condition_for_cloud = common_v2_pb2.VisibilityCondition(
+        visibility_condition_for_cloud = common_pb2.VisibilityCondition(
             condition_field="writerType",
             string_value="Cloud"
         )
 
-        visibility_condition_for_database = common_v2_pb2.VisibilityCondition(
+        visibility_condition_for_database = common_pb2.VisibilityCondition(
             condition_field="writerType",
             string_value="Database"
         )
 
-        visibility_condition_for_file = common_v2_pb2.VisibilityCondition(
+        visibility_condition_for_file = common_pb2.VisibilityCondition(
             condition_field="writerType",
             string_value="File"
         )
@@ -125,37 +125,37 @@ class DestinationImpl(destination_sdk_v2_pb2_grpc.DestinationConnectorServicer):
         # Note: The 'name' and 'label' parameters in the FormField for conditional fields are not used.
 
         # Create conditional fields for Cloud
-        conditional_fields_for_cloud = common_v2_pb2.ConditionalFields(
+        conditional_fields_for_cloud = common_pb2.ConditionalFields(
             condition=visibility_condition_for_cloud,
             fields=[host, port, user, password, region]
         )
 
         # Create conditional fields for File
-        conditional_fields_for_file = common_v2_pb2.ConditionalFields(
+        conditional_fields_for_file = common_pb2.ConditionalFields(
             condition=visibility_condition_for_file,
             fields=[host, port, user, password, table, file_path]
         )
 
         # Create conditional fields for Database
-        conditional_fields_for_database = common_v2_pb2.ConditionalFields(
+        conditional_fields_for_database = common_pb2.ConditionalFields(
             condition=visibility_condition_for_database,
             fields=[host, port, user, password, database, table]
         )
 
         # Add conditional fields to the form
-        conditional_field_for_cloud = common_v2_pb2.FormField(
+        conditional_field_for_cloud = common_pb2.FormField(
             name="conditional_field_for_cloud",
             label="Conditional field for cloud",
             conditional_fields=conditional_fields_for_cloud
         )
 
-        conditional_field_for_file = common_v2_pb2.FormField(
+        conditional_field_for_file = common_pb2.FormField(
             name="conditional_field_for_file",
             label="Conditional field for File",
             conditional_fields=conditional_fields_for_file
         )
 
-        conditional_field_for_database = common_v2_pb2.FormField(
+        conditional_field_for_database = common_pb2.FormField(
             name="conditional_field_for_database",
             label="Conditional field for Database",
             conditional_fields=conditional_fields_for_database
@@ -186,21 +186,21 @@ class DestinationImpl(destination_sdk_v2_pb2_grpc.DestinationConnectorServicer):
     def Test(self, request, context):
         test_name = request.name
         log_message(INFO, "test name: " + test_name)
-        return common_v2_pb2.TestResponse(success=True)
+        return common_pb2.TestResponse(success=True)
 
     def CreateTable(self, request, context):
         print("[CreateTable] :" + str(request.schema_name) + " | " + str(request.table.name) + " | " + str(request.table.columns))
-        return destination_sdk_v2_pb2.CreateTableResponse(success=True)
+        return destination_sdk_pb2.CreateTableResponse(success=True)
 
     def AlterTable(self, request, context):
-        res: destination_sdk_v2_pb2.AlterTableResponse
+        res: destination_sdk_pb2.AlterTableResponse
 
         print("[AlterTable]: " + str(request.schema_name) + " | " + str(request.table.name) + " | " + str(request.table.columns))
-        return destination_sdk_v2_pb2.AlterTableResponse(success=True)
+        return destination_sdk_pb2.AlterTableResponse(success=True)
 
     def Truncate(self, request, context):
         print("[TruncateTable]: " + str(request.schema_name) + " | " + str(request.schema_name) + " | soft" + str(request.soft))
-        return destination_sdk_v2_pb2.TruncateResponse(success=True)
+        return destination_sdk_pb2.TruncateResponse(success=True)
 
     def WriteBatch(self, request, context):
         for replace_file in request.replace_files:
@@ -218,15 +218,15 @@ class DestinationImpl(destination_sdk_v2_pb2_grpc.DestinationConnectorServicer):
             read_csv.decrypt_file(key, value)
         log_message(INFO, "\nData loading completed for table " + request.table.name + "\n")
 
-        res: destination_sdk_v2_pb2.WriteBatchResponse = destination_sdk_v2_pb2.WriteBatchResponse(success=True)
+        res: destination_sdk_pb2.WriteBatchResponse = destination_sdk_pb2.WriteBatchResponse(success=True)
         return res
 
     def DescribeTable(self, request, context):
-        column1 = common_v2_pb2.Column(name="a1", type=common_v2_pb2.DataType.UNSPECIFIED, primary_key=True)
-        column2 = common_v2_pb2.Column(name="a2", type=common_v2_pb2.DataType.DOUBLE)
-        table: common_v2_pb2.Table = common_v2_pb2.Table(name=request.table_name, columns=[column1, column2])
+        column1 = common_pb2.Column(name="a1", type=common_pb2.DataType.UNSPECIFIED, primary_key=True)
+        column2 = common_pb2.Column(name="a2", type=common_pb2.DataType.DOUBLE)
+        table: common_pb2.Table = common_pb2.Table(name=request.table_name, columns=[column1, column2])
         log_message(SEVERE, "Sample severe message: Completed fetching table info")
-        return destination_sdk_v2_pb2.DescribeTableResponse(not_found=False, table=table)
+        return destination_sdk_pb2.DescribeTableResponse(not_found=False, table=table)
 
 def log_message(level, message):
     print(f'{{"level":"{level}", "message": "{message}", "message-origin": "sdk_destination"}}')
@@ -234,7 +234,7 @@ def log_message(level, message):
 if __name__ == '__main__':
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     server.add_insecure_port('[::]:50052')
-    destination_sdk_v2_pb2_grpc.add_DestinationConnectorServicer_to_server(DestinationImpl(), server)
+    destination_sdk_pb2_grpc.add_DestinationConnectorServicer_to_server(DestinationImpl(), server)
     server.start()
     print("Destination gRPC server started...")
     server.wait_for_termination()
